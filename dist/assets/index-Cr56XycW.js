d91affad58bdfe9d8eed598a1832dcf6890b72ea
ga3i3 <admin@MacBook-Pro-admin.local>
2025-05-29 00:14:41 +0500

    frontend build

---
+++ dist/assets/index-Cr56XycW.js
@@ -0,0 +1,1 @@
+import{_ as o,g as r,o as t,r as n}from"./index-BrI144mG.js";const s={};function c(_,a,p,i,f,u){const e=n("router-view");return t(),r(e)}const m=o(s,[["render",c]]);export{m as default};
