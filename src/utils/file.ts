5a445a4033dd61baa8568dde6d4e692c32f691ab
ga3i3 <admin@MacBook-Pro-admin.local>
2025-05-29 00:18:13 +0500

    frontend build 2

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,9 +1,9 @@
 const file = (filename: string) => {
-	return `https://${
-		import.meta.env.VITE_APP_NGROK_SUBDOMAIN
-	}/api/uploads/${filename}`
+	// return `https://${
+	// 	import.meta.env.VITE_APP_NGROK_SUBDOMAIN
+	// }/api/uploads/${filename}`
 
-	// return `${import.meta.env.VITE_APP_HOME_URL}api/uploads/${filename}`
+	return `${import.meta.env.VITE_APP_HOME_URL}api/uploads/${filename}`
 }
 
 export default file
